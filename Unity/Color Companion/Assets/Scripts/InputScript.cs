b703427f101b03e4678a44eea7e79846cf93fd63
shrnkm <sahar.niknam@gmail.com>
2020-04-18 08:48:17 +0200

    update unity files

---
+++ Unity/Color Companion/Assets/Scripts/InputScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputScript : MonoBehaviour
+{
+    
+    private InputField _inputField;
+    
+    
+    void Start()
+    {
+        _inputField = GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if enter key pressed (or send button was clicked)
+        // if text is empty
+    }
+}
