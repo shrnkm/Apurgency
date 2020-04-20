eaaf087bd8ddd02275e79f6352adc4cd8f609b80
shrnkm <sahar.niknam@gmail.com>
2020-04-20 13:02:14 +0200

    update unity

---
+++ Unity/Color Companion/Assets/Scripts/ParserScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParserScript : MonoBehaviour
+{
+    public InputField userIn;
+    private string theInput;
+
+    public void OnSubmit()
+    {
+        theInput = userIn.text;
+        Debug.Log("You wrote" + theInput);
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+
+    }
+}
+ 
